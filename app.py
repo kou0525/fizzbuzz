98674dcde8389633cd0916d8e9aa6c5866166035
kou <goharak@live.jp>
2019-09-30 15:26:10 +0900

    繰り返し文 whileを追加

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-for i in range(1, 101):
-    if i % 15 == 0:
+for number in range(1, 101):
+    if number % 15 == 0:
         print("FizzBuzz")
-    elif i % 3 == 0:
+    elif number % 3 == 0:
         print("Fizz")
-    elif i % 5 == 0:
+    elif number % 5 == 0:
         print("Buzz")
     else:
-        print(i)
+        print(number)
+
+print("======================================================")
+
+number = 1
+
+while number < 101: #numberが101未満（numberが100）であれば下記のif文を実行
+    if number % 15 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
+    number += 1 #numberに+1する
