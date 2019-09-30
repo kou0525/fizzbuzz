9b10c88df8f084f3f2e3f78797fe8309514a971d
kou <goharak@live.jp>
2019-09-30 16:00:46 +0900

    関数化して繰り返し

--- app.py
+++ app.py
@@ -1,24 +1,47 @@
-for number in range(1, 101):
+# for number in range(1, 101):
+#     print("for文")
+#     if number % 15 == 0:
+#         print("FizzBuzz")
+#     elif number % 3 == 0:
+#         print("Fizz")
+#     elif number % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(number)
+
+# ---------------------------------------------
+
+# number = 1
+#
+# while number < 101: #numberが101未満（numberが100）であれば下記のif文を実行
+#     print("while文")
+#     if number % 15 == 0:
+#         print("FizzBuzz")
+#     elif number % 3 == 0:
+#         print("Fizz")
+#     elif number % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(number)
+#     number += 1 #numberに+1する
+#
+
+
+# ---------------------------------------------
+# 関数化
+
+def fizzbuzz_convert(number):
     if number % 15 == 0:
-        print("FizzBuzz")
-    elif number % 3 == 0:
-        print("Fizz")
+        print("Fizz,Buzz")
     elif number % 5 == 0:
         print("Buzz")
-    else:
-        print(number)
-
-print("======================================================")
-
-number = 1
-
-while number < 101: #numberが101未満（numberが100）であれば下記のif文を実行
-    if number % 15 == 0:
-        print("FizzBuzz")
     elif number % 3 == 0:
         print("Fizz")
-    elif number % 5 == 0:
-        print("Buzz")
     else:
-        print(number)
-    number += 1 #numberに+1する
+        print(str(number))
+    return number  # returnが無いとNoneが返ってくるため必要
+
+
+for i in range(1, 101):
+    print(fizzbuzz_convert(i))
+    # print(i)
